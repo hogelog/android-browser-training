96a30a20c7b4566de52f318f3a96a6a0fb7ad4e9
hogelog <konbu.komuro@gmail.com>
2015-03-19 22:29:32 +0900

    Fix inside pattern

--- app/src/main/java/org/hogel/naroubrowser/consts/UrlConst.java
+++ app/src/main/java/org/hogel/naroubrowser/consts/UrlConst.java
@@ -3,7 +3,7 @@ package org.hogel.naroubrowser.consts;
 import java.util.regex.Pattern;
 
 public class UrlConst {
-    public static final Pattern PATTERN_URL_INSIDE = Pattern.compile("^https?://(?:(?:ncode|yomou|ssl|mypage)\\.)?syosetu\\.com(?:$|/.*)");
+    public static final Pattern PATTERN_URL_INSIDE = Pattern.compile("^https?://(?:(?:\\w+)\\.)?syosetu\\.com(?:$|/.*)");
 
     public static final String URL_LAUNCH = "http://syosetu.com/favnovelmain/list/";
 }
