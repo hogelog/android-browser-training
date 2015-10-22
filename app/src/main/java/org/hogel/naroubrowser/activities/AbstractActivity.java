f3eb165e480d9fc224bb1e09448485a758be9dd5
hogelog <konbu.komuro@gmail.com>
2015-10-22 22:23:24 +0900

    Remove RoboActionBarActivity

--- app/src/main/java/org/hogel/naroubrowser/activities/AbstractActivity.java
+++ app/src/main/java/org/hogel/naroubrowser/activities/AbstractActivity.java
@@ -1,6 +1,42 @@
 package org.hogel.naroubrowser.activities;
 
-import roboguice.activity.RoboActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.google.inject.Key;
+import roboguice.RoboGuice;
+import roboguice.inject.RoboInjector;
+import roboguice.util.RoboContext;
 
-public class AbstractActivity extends RoboActionBarActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class AbstractActivity extends AppCompatActivity implements RoboContext {
+    protected HashMap<Key<?>,Object> scopedObjects = new HashMap<>();
+
+    @Override
+    public Map<Key<?>, Object> getScopedObjectMap() {
+        return scopedObjects;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final RoboInjector injector = RoboGuice.getInjector(this);
+        injector.injectMembersWithoutViews(this);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+        RoboGuice.getInjector(this).injectViewMembers(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        try {
+            RoboGuice.destroyInjector(this);
+        } finally {
+            super.onDestroy();
+        }
+    }
 }
