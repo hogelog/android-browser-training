4015a7d097fc62bbd59e43b46ee1628c73857ba4
hogelog <konbu.komuro@gmail.com>
2016-01-10 05:17:16 +0900

    Upgrade libraries

--- app/src/main/java/org/hogel/naroubrowser/database/BrowserDatabaseHelper.java
+++ app/src/main/java/org/hogel/naroubrowser/database/BrowserDatabaseHelper.java
@@ -4,14 +4,14 @@ import android.content.Context;
 import android.content.res.AssetManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import com.google.common.base.Charsets;
-import com.google.common.io.CharStreams;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
 @Singleton
@@ -19,6 +19,7 @@ public class BrowserDatabaseHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "application.db";
     private static final int DB_VERSION = 1;
     private static final String MIGRATION_DIR = "db/migration";
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
 
     @Inject
     AssetManager assetManager;
@@ -51,8 +52,8 @@ public class BrowserDatabaseHelper extends SQLiteOpenHelper {
     }
 
     private void executeSql(SQLiteDatabase db, String path) throws IOException {
-        try (Reader reader = new InputStreamReader(assetManager.open(path, AssetManager.ACCESS_BUFFER), Charsets.UTF_8)) {
-            String sql = CharStreams.toString(reader);
+        try (Reader reader = new InputStreamReader(assetManager.open(path, AssetManager.ACCESS_BUFFER), UTF_8)) {
+            String sql = IOUtils.toString(reader);
             db.execSQL(sql);
         }
     }
